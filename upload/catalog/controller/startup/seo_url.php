ae155bbaec613175bd24979a65009e2da05b40aa
Adam Lanning-Molyneux <adam@mediaorb.co.uk>
2022-11-09 12:13:56 +0000

    Update seo_url.php
    Allows URLs without new params, remove sorting of URLs alphabetically -.-

--- upload/catalog/controller/startup/seo_url.php
+++ upload/catalog/controller/startup/seo_url.php
@@ -1,5 +1,6 @@
 <?php
 namespace Opencart\Catalog\Controller\Startup;
+use \Opencart\System\Helper as Helper;
 class SeoUrl extends \Opencart\System\Engine\Controller {
 	public function index(): void {
 		// Add rewrite to URL class
@@ -14,7 +15,7 @@ class SeoUrl extends \Opencart\System\Engine\Controller {
 			$parts = explode('/', $this->request->get['_route_']);
 
 			// remove any empty arrays from trailing
-			if (oc_strlen(end($parts)) == 0) {
+			if (Helper\Utf8\strlen(end($parts)) == 0) {
 				array_pop($parts);
 			}
 
@@ -23,6 +24,21 @@ class SeoUrl extends \Opencart\System\Engine\Controller {
 
 				if ($seo_url_info) {
 					$this->request->get[$seo_url_info['key']] = html_entity_decode($seo_url_info['value'], ENT_QUOTES, 'UTF-8');
+
+					if($seo_url_info['key'] == 'product_id'){
+						$this->request->get['route'] = 'product/product';
+						if(empty($this->request->get['language'])){
+							$this->request->get['language'] = 'en-gb';
+						}
+					}
+					
+					if($seo_url_info['key'] == 'path'){
+						$this->request->get['route'] = 'product/category';
+						if(empty($this->request->get['language'])){
+							$this->request->get['language'] = 'en-gb';
+						}
+					}
+
 				}
 			}
 		}
@@ -59,7 +75,7 @@ class SeoUrl extends \Opencart\System\Engine\Controller {
 		foreach ($parts as $part) {
 			[$key, $value] = explode('=', $part);
 
-			$result = $this->model_design_seo_url->getSeoUrlByKeyValue((string)$key, (string)$value);
+			$result = $this->model_design_seo_url->getSeoUrlByKeyValue($key, $value);
 
 			if ($result) {
 				$paths[] = $result;
@@ -74,10 +90,11 @@ class SeoUrl extends \Opencart\System\Engine\Controller {
 			$sort_order[$key] = $value['sort_order'];
 		}
 
-		array_multisort($sort_order, SORT_ASC, $paths);
+		//array_multisort($sort_order, SORT_ASC, $paths);
 
 		// Build the path
 		$url .= str_replace('/index.php', '', $url_info['path']);
+
 
 		foreach ($paths as $result) {
 			$url .= '/' . $result['keyword'];
@@ -85,7 +102,7 @@ class SeoUrl extends \Opencart\System\Engine\Controller {
 
 		// Rebuild the URL query
 		if ($query) {
-			$url .= '?' . str_replace(['%2F'], ['/'], http_build_query($query));
+			$url .= '?' . str_replace(['%2F', '%7C'], ['/', '|'], http_build_query($query));
 		}
 
 		return $url;
